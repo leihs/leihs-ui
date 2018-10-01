097a981b463b1e1deaed96fe58b5a57fd0f36742
Max Albrecht <max.albrecht@zhdk.ch>
2018-10-01 10:19:43 +0200

    add dummy example page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
+import Link from "next/link";
+import LeihsPage from "../components/leihs-page";
+
 export default () => {
-  return "Hello World";
+  return (
+    <LeihsPage>
+      <div className="container p-4">
+        <h1>Hello World!</h1>
+      </div>
+    </LeihsPage>
+  );
 };
