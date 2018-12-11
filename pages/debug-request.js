ee116ad287fc022d9065422b136522f8f92787f8
Max Albrecht <max.albrecht@zhdk.ch>
2018-12-11 15:43:32 +0100

    better request debug page

--- pages/debug-request.js
+++ pages/debug-request.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React from 'react'
+import f from 'lodash'
 
 export default context => {
   return (
-    <div className="container p-4">
-      <h1>debug request</h1>
-      <mark>
-        <small>
-          this page is shown either shown because a non-<code>GET</code>-request
-          was sent from the browser or it navigated to{" "}
-          <code>
-            <a href="/debug-request">/debug-request</a>
-          </code>
-        </small>
-      </mark>
-      <pre>{JSON.stringify({ context }, 0, 2)}</pre>
+    <div className="container p-1">
+      <div className="bg-white card p-4">
+        <h1>debug request</h1>
+        <mark>
+          <small>
+            this page is shown either shown because a non-
+            <code>GET</code>
+            -request was sent from the browser or it navigated to the URL{' '}
+            <code>
+              <a href="/debug-request">/debug-request</a>
+            </code>
+            .
+          </small>
+        </mark>
+        <hr />
+        <h3 className="h5">HTTP request as received on the server:</h3>
+        <pre>
+          <b>
+            {JSON.stringify(
+              f.get(context, ['url', 'query', 'requestAsSeenOnServer']),
+              0,
+              2
+            ) || 'error'}
+          </b>
+        </pre>
+      </div>
     </div>
-  );
-};
+  )
+}
