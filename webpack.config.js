bc15d102dc46c3c0e5efd461c2289d679158094a
Max Albrecht <max.albrecht@zhdk.ch>
2020-05-20 10:58:46 +0200

    lib: do not include react in bundle
    if the library included react, and is then bundled again with shadow-cljs,
    then 2 copies of react will end up in the resulting application bundle
    (one from the library, one from reagent).
    
    this is bad for bundle size, possible version mismatches,
    but most importantly it breaks react hooks!

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,28 @@ module.exports = Object.assign({}, createReactAppConfig, {
     'leihs-ui-client-side': './src/client-side.js',
     'leihs-ui-server-side': './src/server-side.js'
   },
+
   output: Object.assign({}, createReactAppConfig.output, {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
-    // library: "LeihsUI",
+    // library: 'LeihsUI',
     libraryTarget: 'umd'
   }),
+
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom'
+    }
+  },
 
   // DISABLE CHUNKS…
   optimization: {}
