4232f38a9caaaf0ab240568291de97b48ace1ebe
uvb <urs.vanbinsbergen@zhdk.ch>
2021-08-18 17:27:11 +0200

    PropertyTable: top align and improved cell widths

--- src/components/MobileApp/DesignComponents/PropertyTable.stories.js
+++ src/components/MobileApp/DesignComponents/PropertyTable.stories.js
@@ -22,6 +22,74 @@ export const keyValueTable = () => {
 }
 keyValueTable.storyName = 'PropertyTable'
 
+export const withLongContent = () => {
+  return (
+    <div>
+      <h1>PropertyTable</h1>
+      <p className="text-muted">Short keys, long content</p>
+      <PropertyTable
+        className="shadow"
+        style={{ maxWidth: '40rem' }}
+        properties={[
+          {
+            key: 'Key 1',
+            value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et maiores cumque in nisi natus id saepe!'
+          },
+          {
+            key: 'Key 2',
+            value: 'Et maiores cumque in nisi natus id saepe!'
+          }
+        ]}
+      />
+      <p></p>
+      <p className="text-muted">Long keys, short content</p>
+      <PropertyTable
+        className="shadow"
+        style={{ maxWidth: '40rem' }}
+        properties={[
+          {
+            key: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et maiores cumque in nisi natus id saepe!',
+            value: 'Value 1'
+          },
+          {
+            key: 'Et maiores cumque in nisi natus id saepe!',
+            value: 'Value 2'
+          }
+        ]}
+      />
+      <p></p>
+      <p className="text-muted">Both mixed</p>
+      <PropertyTable
+        className="shadow"
+        style={{ maxWidth: '40rem' }}
+        properties={[
+          {
+            key: 'Key 1',
+            value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et maiores cumque in nisi natus id saepe!'
+          },
+          {
+            key: 'Et maiores cumque in nisi natus id saepe!',
+            value: 'Value 2'
+          }
+        ]}
+      />
+      <p></p>
+      <p className="text-muted">Long words</p>
+      <PropertyTable
+        className="shadow"
+        style={{ maxWidth: '20rem' }}
+        properties={[
+          {
+            key: 'supercalifragilisticexpialidocious',
+            value: 'supercalifragilisticexpialidocious'
+          }
+        ]}
+      />
+      <p></p>
+    </div>
+  )
+}
+
 export const restProps = () => {
   return (
     <div>
