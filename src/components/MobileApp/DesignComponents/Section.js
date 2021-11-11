25a636f8898ac20e741d92a03cd427d88033d1d5
uvb <urs.vanbinsbergen@zhdk.ch>
2021-11-11 12:19:10 +0100

    add Section.Controlled

--- src/components/MobileApp/DesignComponents/Section.js
+++ src/components/MobileApp/DesignComponents/Section.js
@@ -2,22 +2,54 @@ import React, { useState } from 'react'
 import cx from 'classnames'
 import Icon, { iconSectionArrow } from './Icons'
 
-export default function Section({ title, children, collapsible, defaultCollapsed, className, ...restProps }) {
-  const [collapsed, setCollapsed] = useState(defaultCollapsed)
+export default function Section({
+  title,
+  children,
+  collapsible,
+  initialCollapsed,
+  onToggleCollapse,
+  className,
+  ...restProps
+}) {
+  const [collapsed, setCollapsed] = useState(initialCollapsed)
 
+  const toggleCollapse = nextCollapsed => {
+    setCollapsed(nextCollapsed)
+    onToggleCollapse && onToggleCollapse(nextCollapsed)
+  }
+
+  return (
+    <ControlledSection
+      title={title}
+      collapsible={collapsible}
+      collapsed={collapsed}
+      onToggleCollapse={toggleCollapse}
+      className={className}
+      {...restProps}
+    >
+      {children}
+    </ControlledSection>
+  )
+}
+
+function ControlledSection({ title, children, collapsible, collapsed, onToggleCollapse, className, ...restProps }) {
   const toggle = () => {
     if (collapsible) {
-      setCollapsed(x => !x)
+      onToggleCollapse(!collapsed)
     }
   }
 
   return (
     <section className={cx(className)} {...restProps}>
-      {(title || collapsed) && (
+      {(title || collapsible) && (
         <h2 className={cx('section-title', 'fw-normal')}>
           <span
             onClick={toggle}
-            className={cx({ 'section-title__collapser': collapsible })}
+            className={cx({
+              'section-title__collapser': collapsible,
+              'ui-section-expander': collapsible && collapsed,
+              'ui-section-collapser': collapsible && !collapsed
+            })}
             role={collapsible ? 'button' : ''}
           >
             {title}
@@ -37,3 +69,5 @@ export default function Section({ title, children, collapsible, defaultCollapsed
     </section>
   )
 }
+
+Section.Controlled = ControlledSection
