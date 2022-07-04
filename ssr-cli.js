87b20ffd2803654b2cc6adb8a80a226404c1a97d
uvb <urs.vanbinsbergen@zhdk.ch>
2022-07-04 17:01:35 +0200

    update npm packages

--- ssr-cli.js
+++ ssr-cli.js
@@ -1,7 +1,7 @@
 // Source of mini CLI that is called from server process by pipeing the
 // bundled source to stdin and call it with positional arguments.
 // For minimal version of how it used see ../test/test-ssr-call.sh
-// This CLI is bundled into a single file with <http://npmjs.com/@zeit/ncc>.
+// This CLI is bundled into a single file with <http://npmjs.com/@vercel/ncc>.
 
 const LeihsUI = require('./dist/leihs-ui-server-side')
 
