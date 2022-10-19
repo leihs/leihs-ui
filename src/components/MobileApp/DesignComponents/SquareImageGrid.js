0a92557f7266810826a5878d82f208b01d070756
uvb <urs.vanbinsbergen@zhdk.ch>
2022-10-19 18:01:11 +0200

    add "ui-" classes to `SquareImageGrid`

--- src/components/MobileApp/DesignComponents/SquareImageGrid.js
+++ src/components/MobileApp/DesignComponents/SquareImageGrid.js
@@ -5,9 +5,9 @@ import SquareImage from './SquareImage'
 
 export default function SquareImageGrid({ list = [], className, itemClassName, ...restProps }) {
   return (
-    <div className={cx('row g-0 d-flex flex-wrap mx-n2', className)} {...restProps}>
+    <div className={cx('ui-square-image-grid row g-0 d-flex flex-wrap mx-n2', className)} {...restProps}>
       {list.map(({ id, href, imgSrc, placeholder, caption, subCaption, isDimmed, isFavorited }) => (
-        <div key={id} className="col-6 col-sm-3" style={{ opacity: isDimmed ? 0.35 : 1 }}>
+        <div key={id} className="ui-square-image-grid-item col-6 col-sm-3" style={{ opacity: isDimmed ? 0.35 : 1 }}>
           <div
             className={cx('max-w-sm overflow-hidden bg-white mx-2 mb-3 position-relative', itemClassName)}
             style={{ opacity: 1 }}
@@ -20,8 +20,8 @@ export default function SquareImageGrid({ list = [], className, itemClassName, .
             <SquareImage href={href} imgSrc={imgSrc} placeholder={placeholder} className="rounded border" />
             <div className="pt-2">
               <a className="stretched-link" href={href}>
-                <div className="small">{caption}</div>
-                <div className="small fw-light">{subCaption}</div>
+                <div className="ui-caption small">{caption}</div>
+                <div className="ui-sub-caption small fw-light">{subCaption}</div>
               </a>
             </div>
           </div>
